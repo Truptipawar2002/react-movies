d8822c8257a5dd0bcdfc9961619fc24f901a0da7
Trupti pawar <pawartrupti2002@gmail.com>
2025-05-15 13:20:43 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+ import Search from './components/Search'
+
+
+const App = () => {
+  return (
+    <main>
+    <div className='pattern' /> 
+
+     <div className='wrapper'>
+      <header>
+        <img src='./hero.png' alt='Hero Banner'/>
+        <h1>find <span className='text-gradient'>Movies</span> you'll Enjoy Wihtout the Hassle</h1>
+      </header>
+      <Search />
+     </div>
+  </main>
+  )
+}
+
+export default App
